356fbbf20c665359bf615b8745a6d8d7b453fb54
Johannes Konings <johannes.konings@outlook.com>
2021-11-01 17:45:17 +0000

    add crawler

--- lib/aws-cdk-glue-crawler-stack.ts
+++ lib/aws-cdk-glue-crawler-stack.ts
@@ -1,9 +1,38 @@
 import * as cdk from '@aws-cdk/core';
+import * as glue from '@aws-cdk/aws-glue'
+import * as iam from '@aws-cdk/aws-iam'
+import * as s3 from '@aws-cdk/aws-s3'
 
 export class AwsCdkGlueCrawlerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const name = 'cdk-glue-crawler'
+
+    const glueDb = new glue.Database(this, 'glue db', {
+      databaseName: `${name}-db`,
+    })
+
+    const s3Bucket = new s3.Bucket(this, 's3-bucket', {
+      bucketName: `${name}-bucket`,
+    })
+
+    const roleCrawler = new iam.Role(this, 'role crawler', {
+      roleName: `${name}-role`,
+      assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
+    })
+
+    const glueCrawler = new glue.CfnCrawler(this, 'glue crawler', {
+      role: roleCrawler.roleArn,
+      targets: {
+        s3Targets: [
+          {
+            path: `s3://${s3Bucket.bucketName}`,
+          },
+        ],
+      },
+      databaseName: glueDb.databaseName
+    })
+
   }
 }
